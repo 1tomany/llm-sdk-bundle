69d8517a45a6c63ef42bb7cbe410f25a37277fe2
Vic Cherubini <vcherubini@gmail.com>
2026-02-06 16:23:46 -0600

    wip

--- src/PhpAiBundle.php
+++ src/PhpAiBundle.php
@@ -2,8 +2,6 @@
 
 namespace OneToMany\PhpAiBundle;
 
-use OneToMany\AI\Client\Gemini\FileClient as GeminiFileClient;
-use OneToMany\AI\Client\OpenAi\FileClient as OpenAiFileClient;
 use OneToMany\AI\Contract\Client\FileClientInterface;
 use OneToMany\AI\Contract\Client\QueryClientInterface;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
@@ -13,10 +11,27 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class PhpAiBundle extends AbstractBundle
 {
+    /**
+     * @var array{
+     *   gemini: non-empty-list<class-string>,
+     *   openai: non-empty-list<class-string>,
+     * }
+     */
+    private array $clients = [
+        'gemini' => [
+            \OneToMany\AI\Client\Gemini\FileClient::class,
+            \OneToMany\AI\Client\Gemini\QueryClient::class,
+        ],
+        'openai' => [
+            \OneToMany\AI\Client\OpenAi\FileClient::class,
+            \OneToMany\AI\Client\OpenAi\QueryClient::class,
+        ],
+    ];
+
     public function configure(DefinitionConfigurator $definition): void
     {
         /**
-         * @disregard P1013 Undefined method
+         * @disregard P1013 Undefined method rootNode()
          */
         $definition
             ->rootNode()
@@ -43,34 +58,39 @@ class PhpAiBundle extends AbstractBundle
     }
 
     /**
-     * @param array<string, mixed> $config
+     * @param array{
+     *   gemini: array{
+     *     api_key?: non-empty-string,
+     *     enabled: bool,
+     *   },
+     *   openai: array{
+     *     api_key?: non-empty-string,
+     *     enabled: bool,
+     *   }
+     * } $config
      */
     public function loadExtension(array $config, ContainerConfigurator $configurator, ContainerBuilder $builder): void
     {
-        // $builder
-        //     ->registerForAutoconfiguration(FileClientInterface::class)
-        //     ->addTag('1tomany.ai.file_client');
+        $builder
+            ->registerForAutoconfiguration(FileClientInterface::class)
+            ->addTag('1tomany.ai.file_client');
 
-        // $builder
-        //     ->registerForAutoconfiguration(QueryClientInterface::class)
-        //     ->addTag('1tomany.ai.query_client');
+        $builder
+            ->registerForAutoconfiguration(QueryClientInterface::class)
+            ->addTag('1tomany.ai.query_client');
 
-        if ($config['gemini']['enabled']) {
-            $definition = $builder->register(GeminiFileClient::class);
+        foreach ($config as $platform => $settings) {
+            $apiKey = $settings['api_key'] ?? null;
 
-            $definition
-                ->setAutoconfigured(true)
-                ->setAutowired(true)
-                ->setArgument('$apiKey', $config['gemini']['api_key'])
-                ->addTag('1tomany.ai.file_client');
-        }
-
-        if ($config['openai']['enabled']) {
-            $builder->register(OpenAiFileClient::class)
-                ->setArgument('$apiKey', $config['openai']['api_key'])
-                ->setAutoconfigured(true)
-                ->setAutowired(true)
-                ->addTag('1tomany.ai.file_client');
+            if ($settings['enabled'] && null !== $apiKey) {
+                foreach ($this->clients[$platform] as $clientClass) {
+                    $builder
+                        ->register($clientClass)
+                        ->setAutowired(true)
+                        ->setAutoconfigured(true)
+                        ->setArgument('$apiKey', $apiKey);
+                }
+            }
         }
 
         $configurator->import('../config/services.yaml');

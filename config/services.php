dd0db06d6f6182a349ba7f54e39456b623e84f74
Vic Cherubini <vcherubini@gmail.com>
2026-02-20 18:02:01 -0600

    wip

--- config/services.php
+++ config/services.php
@@ -1,21 +1,21 @@
 <?php
 
-use OneToMany\AI\Clients\Action\File\DeleteFileAction;
-use OneToMany\AI\Clients\Action\File\UploadFileAction;
-use OneToMany\AI\Clients\Action\Query\CompileQueryAction;
-use OneToMany\AI\Clients\Action\Query\ExecuteQueryAction;
-use OneToMany\AI\Clients\Client\Claude\FileClient as ClaudeFileClient;
-use OneToMany\AI\Clients\Client\Gemini\FileClient as GeminiFileClient;
-use OneToMany\AI\Clients\Client\Gemini\QueryClient as GeminiQueryClient;
-use OneToMany\AI\Clients\Client\Mock\FileClient as MockFileClient;
-use OneToMany\AI\Clients\Client\Mock\QueryClient as MockQueryClient;
-use OneToMany\AI\Clients\Client\OpenAI\FileClient as OpenAIFileClient;
-use OneToMany\AI\Clients\Client\OpenAI\QueryClient as OpenAIQueryClient;
-use OneToMany\AI\Clients\Contract\Action\File\DeleteFileActionInterface;
-use OneToMany\AI\Clients\Contract\Action\File\UploadFileActionInterface;
-use OneToMany\AI\Clients\Contract\Action\Query\CompileQueryActionInterface;
-use OneToMany\AI\Clients\Contract\Action\Query\ExecuteQueryActionInterface;
-use OneToMany\AI\Clients\Factory\ClientFactory;
+use OneToMany\LlmSdk\Action\File\DeleteFileAction;
+use OneToMany\LlmSdk\Action\File\UploadFileAction;
+use OneToMany\LlmSdk\Action\Query\CompileQueryAction;
+use OneToMany\LlmSdk\Action\Query\ExecuteQueryAction;
+use OneToMany\LlmSdk\Client\Claude\FileClient as ClaudeFileClient;
+use OneToMany\LlmSdk\Client\Gemini\FileClient as GeminiFileClient;
+use OneToMany\LlmSdk\Client\Gemini\QueryClient as GeminiQueryClient;
+use OneToMany\LlmSdk\Client\Mock\FileClient as MockFileClient;
+use OneToMany\LlmSdk\Client\Mock\QueryClient as MockQueryClient;
+use OneToMany\LlmSdk\Client\OpenAI\FileClient as OpenAIFileClient;
+use OneToMany\LlmSdk\Client\OpenAI\QueryClient as OpenAIQueryClient;
+use OneToMany\LlmSdk\Contract\Action\File\DeleteFileActionInterface;
+use OneToMany\LlmSdk\Contract\Action\File\UploadFileActionInterface;
+use OneToMany\LlmSdk\Contract\Action\Query\CompileQueryActionInterface;
+use OneToMany\LlmSdk\Contract\Action\Query\ExecuteQueryActionInterface;
+use OneToMany\LlmSdk\Factory\ClientFactory;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
